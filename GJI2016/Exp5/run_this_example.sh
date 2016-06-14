4d79f2ac199dfd349123ff8c15fed184eaae4d59
yanhuay <yanhuay@princeton.edu>
2016-06-14 17:52:51 -0400

    update GJI2016 examples

--- GJI2016/Exp5/run_this_example.sh
+++ GJI2016/Exp5/run_this_example.sh
@@ -3,7 +3,6 @@
 source parameter
 
 echo "Run this example ..."
-echo 'will take 1 h ...'
 
 rm -rf submit_job
 
@@ -12,7 +11,9 @@ mkdir submit_job
 cp -r $package_path/scripts/submit.sh submit_job/
 cp -r DATA submit_job/
 cp -r parameter submit_job/
-cp -r SU_process submit_job/
+if [ -d "SU_process" ]; then
+    cp -r SU_process submit_job/
+fi
 
 echo 'cd submit_job/'
 echo './submit.sh'
