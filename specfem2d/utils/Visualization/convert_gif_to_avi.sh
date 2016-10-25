df5c6bf2ce6b1b81c56dc55ec2f5e86171674f9f
yanhuay <yanhuay@princeton.edu>
2016-10-25 15:22:58 -0400

    remove commits in masters

--- specfem2d/utils/Visualization/convert_gif_to_avi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for file in imag*.gif; do
-  jfile=`echo $file | sed 's/gif/jpg/'`
-  convert -quality 100 $file $jfile
-  echo convert -quality 100 $file $jfile
-done
-mencoder "mf://imag*.jpg" -mf fps=1 -o output.avi -ovc lavc -lavcopts vcodec=mpeg4
--- specfem2d/utils/small_SEM_solver_in_Fortran_without_MPI_to_learn/plotall.gnu
+++
@@ -1,6 +0,0 @@
-
-#set xrange [0:2000]
-
-plot "seismogram.txt" w l lc 1
-pause -1 "Hit any key..."
-
